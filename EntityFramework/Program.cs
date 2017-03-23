c044ff638b763942f4d024a222a9c8960da28b77
Pugal <Pugal@PUGALENTHI>
2017-03-23 16:59:09 +0530

    DBFirst

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntityFramework
 {
@@ -10,6 +6,11 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
+            var vidzyDbContext = new VidzyDbContext();
+
+            vidzyDbContext.AddVideo("Test", DateTime.Today, "Comedy");
+
+
         }
     }
 }
