38d482ab7a601299fd2495b799b9b40206e79750
Pugal <Pugal@PUGALENTHI>
2017-03-24 19:46:09 +0530

    FluentApi

--- CodeFirst/Course.cs
+++ CodeFirst/Course.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CodeFirst
 {
@@ -12,12 +10,8 @@ namespace CodeFirst
         }
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(255)]
         public string Name { get; set; }
 
-        [Required]
-        [MaxLength(2000)]
         public string Description { get; set; }
 
         public int Level { get; set; }
@@ -26,9 +20,10 @@ namespace CodeFirst
 
         public Author Author { get; set; }
 
-        [ForeignKey("Author")]
         public int AuthorId { get; set; }
 
         public ICollection<Tag> Tags { get; set; }
+
+        public Cover Cover { get; set; }
     }
 }
--- CodeFirst/PlutoContext.cs
+++ CodeFirst/PlutoContext.cs
@@ -2,18 +2,46 @@
 
 namespace CodeFirst
 {
-    public class PlutoContext:DbContext
+    public class PlutoContext : DbContext
     {
+        public PlutoContext()
+            : base("name=PlutoCodeFirstFluentAPI")
+        {
 
-        public DbSet<Video> Videos { get; set; }
-        public DbSet<Genre> Genres { get; set; }
+        }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Tag> Tags { get; set; }
-        public PlutoContext()
-            :base("name=PlutoCodeFirst")
-        {
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(M => M.ToTable("CourseTags"));
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+                
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
