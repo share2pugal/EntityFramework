00a681886a510fb6feba365bb15cd222aa6007f7
Pugal <Pugal@PUGALENTHI>
2017-03-28 15:06:10 +0530

    Q

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -31,6 +31,7 @@ namespace Queries
                 System.Console.WriteLine(item.Name);
             }
 
+            // LINQ Syntax
 
             // Query Filter
 
@@ -105,7 +106,7 @@ namespace Queries
             var q2 =
                 from a in conetxt.Authors
                 join c in conetxt.Courses on a.Id equals c.AuthorId into g
-                select new { AuthorName = a.Name,Courses= g.Count() };
+                select new { AuthorName = a.Name, Courses = g.Count() };
 
             //Cross Join
 
@@ -113,6 +114,96 @@ namespace Queries
                 from c in conetxt.Courses
                 from a in conetxt.Authors
                 select new { CourseName = c.Name, AuthorName = a.Name };
+
+
+            //Extention methods
+            //filter
+
+            var courcess = conetxt.Courses.Where(c => c.Level == 1);
+
+            //ordering
+
+            var ordercource = conetxt.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenByDescending(c => c.Level);
+
+            //projection
+
+            var projectionCource = conetxt.Courses.Where(c => c.Level == 1)
+                .Select(c => new { CourseName = c.Name, AuthorName = c.Author.Name });
+
+            var tags = conetxt.Courses.Where(c => c.Level == 1)
+                .SelectMany(c => c.Tags);
+
+            //distinct
+
+            var dis = conetxt.Courses.Where(c => c.Level == 1)
+                .Distinct();
+
+
+            //groupby
+
+            var gr = conetxt.Courses.GroupBy(c => c.Level);
+
+            foreach (var item in gr)
+            {
+                System.Console.WriteLine(item.Key);
+                foreach (var ite in item)
+                {
+                    System.Console.WriteLine("\t" + ite.Name);
+                }
+            }
+            //Inner Join
+
+            conetxt.Courses.Join(conetxt.Authors
+                , c => c.AuthorId
+                , a => a.Id
+                , (course, author) => new
+                {
+                    CourseName = course.Name,
+                    AuthorName = author.Name
+                });
+            //Group Join
+
+            conetxt.Authors.GroupJoin(conetxt.Courses, a => a.Id, c => c.AuthorId, (author, cous) => new
+            {
+
+                AuthorName = author.Name,
+                courses = cous.Count()
+            });
+
+            //Crossjoin
+
+            conetxt.Authors.SelectMany(c => conetxt.Courses, (author, course) => new
+            {
+                authorName = author.Name,
+                courseName = course.Name
+            });
+
+            //Portitioning
+
+            var coursesee = conetxt.Courses.Skip(10).Take(10);
+
+            //Element oper
+
+            var cq = conetxt.Courses.First();
+            var b = conetxt.Courses.FirstOrDefault();
+            var ag = conetxt.Courses.FirstOrDefault(ccc => ccc.FullPrice > 10);
+            var da = conetxt.Courses.Single(co => co.Id == 1);
+            var e = conetxt.Courses.SingleOrDefault(co => co.Id == 1);
+
+            var aa = conetxt.Courses.All(cs => cs.FullPrice > 10);
+            var bb = conetxt.Courses.Any(cc => cc.FullPrice > 10);
+
+            //aggr
+
+            conetxt.Courses.Count();
+            conetxt.Courses.Max(cc => cc.FullPrice);
+            conetxt.Courses.Max(cc => cc.FullPrice);
+            conetxt.Courses.Average(cc => cc.FullPrice);
+
+
 
         }
     }
